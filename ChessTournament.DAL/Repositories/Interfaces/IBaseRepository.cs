3a6f7e12c300aed04c590d7cd084f387d13458a6
makar <saiirko@outlook.fr>
2024-12-11 09:19:56 +0100

    Add Base repo interface

---
+++ ChessTournament.DAL/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessTournament.DAL.Repositories.Interface
+{
+    public interface IBaseRepository<T> 
+    {
+        Task<T> AddAsync(T entity);
+        Task<T?> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+    }
+}
